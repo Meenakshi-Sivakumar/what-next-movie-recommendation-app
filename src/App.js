26c14ee8225b9457609c4b9b1d718286aaa06fea
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2024-07-24 11:25:28 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="bg-blue-500 text-white p-4">
         <h1 className="text-3xl font-bold">Welcome to Movie Search App</h1>
+        <p></p>
       </header>
     </div>
   );
